17cf70359b8b0b8dc5d106e7e8874dc00e154650
armonova <arthurnovaes0@gmail.com>
2021-06-18 18:49:20 -0300

    ex1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,6 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+function openParagraph(idElement) {
+    document.getElementById(idElement).classList.add('expandido');
+}
