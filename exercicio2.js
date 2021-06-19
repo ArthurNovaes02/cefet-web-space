4b953063f7bacf21896f7aa6d529553ba4f6c1ac
armonova <arthurnovaes0@gmail.com>
2021-06-19 12:38:16 -0300

    finished ex2

--- exercicio2.js
+++ exercicio2.js
@@ -1,6 +1,10 @@
 // Faça o exercício da galeria de imagens aqui
 // Este arquivo ainda não está incluído no arquivo HTML
 
+let urlImg = 'http://fegemo.github.io/cefet-web/images/philae-parts.jpg';
+document.getElementById('slide').src = urlImg;
+let index = 0;
+
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
   todasAsImagens = [
     'philae-parts.jpg',
@@ -9,3 +13,21 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+function nextImg(idImg) {
+  index++;
+  if (index >= todasAsImagens.length) {
+    index = 0;
+  }
+  urlImg = servidorDasImagens + todasAsImagens[index];
+  document.getElementById(idImg).src = urlImg;
+}
+
+function prevImg(idImg) {
+  index--;
+  if (index < 0) {
+    index = todasAsImagens.length - 1;
+  }
+  urlImg = servidorDasImagens + todasAsImagens[index];
+  document.getElementById(idImg).src = urlImg;
+}
